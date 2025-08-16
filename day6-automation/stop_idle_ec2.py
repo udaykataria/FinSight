8aa88f69bcbc79dd74ab0492c44a01b5266f4221
Uday Kataria <129209259+udaykataria@users.noreply.github.com>
2025-08-16 14:35:08 +0530

    Create stop_idle_ec2.py

---
+++ day6-automation/stop_idle_ec2.py
@@ -0,0 +1,17 @@
+import boto3
+
+ec2 = boto3.client('ec2')
+
+def lambda_handler(event, context):
+    instances = ec2.describe_instances(
+        Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]
+    )
+
+    stopped = []
+    for reservation in instances['Reservations']:
+        for instance in reservation['Instances']:
+            instance_id = instance['InstanceId']
+            ec2.stop_instances(InstanceIds=[instance_id])
+            stopped.append(instance_id)
+
+    return {"StoppedInstances": stopped}
